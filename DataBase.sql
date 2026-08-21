a49411f3d842fa2367d4e82d7da7730d42c51f5e
yuhasproSoftwareTester <yuhaspro404@gmail.com>
2026-08-21 17:14:20 +0530

    added some bug

--- DataBase.sql
+++ DataBase.sql
@@ -4,7 +4,7 @@ USE TestingForm;
 
 CREATE TABLE users (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(50) NOT NULL,
+    first_name VARCHAR(50) ,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
